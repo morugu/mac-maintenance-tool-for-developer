5356ad43dd8c148725fdf94ea5692d65269c40c7
shoya <12c1055@gmail.com>
2016-11-18 21:50:35 +0900

    Create new project

---
+++ DCleanUp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DCleanUp
+//
+//  Created by shoya on 2016/11/18.
+//  Copyright © 2016 Shoya Shiraki. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DCleanUp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DCleanUp
+//
+//  Created by shoya on 2016/11/18.
+//  Copyright © 2016 Shoya Shiraki. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
