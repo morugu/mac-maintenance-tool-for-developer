60948cac0798612c5ac81247a1c596fcc5fef109
shoya <12c1055@gmail.com>
2016-11-21 23:26:50 +0900

    Add call commad test

--- DCleanUp/AppDelegate.swift
+++ DCleanUp/AppDelegate.swift
@@ -15,12 +15,20 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         // Insert code here to initialize your application
+        shell()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    func shell(_ args: String...) -> Int32 {
+        let task = Process()
+        task.launchPath = "/usr/bin/env"
+        task.arguments = args
+        task.launch()
+        task.waitUntilExit()
+        return task.terminationStatus
+    }
 }
 
