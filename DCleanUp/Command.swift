4543267c5e4126e282142b5adb1081e9d6640f2f
shoya <12c1055@gmail.com>
2016-11-25 23:50:37 +0900

    Add root execute

--- DCleanUp/Command.swift
+++ DCleanUp/Command.swift
@@ -10,7 +10,8 @@ import Cocoa
 
 enum CommandType: String {
     case
-    Env = "env"
+    Env = "/usr/bin/env",
+    Purge = "/usr/sbin/purge"
 }
 
 class Command {
@@ -20,10 +21,14 @@ class Command {
     static func execute(type: CommandType) -> Int {
         print(type.rawValue)
         
+        // TODO: http://stackoverflow.com/questions/26707557/how-to-execute-a-shell-command-with-root-permission-from-swift
+        NSAppleScript(source: "do shell script \"sudo \(type.rawValue)\" with administrator " +
+            "privileges")!.executeAndReturnError(nil)
+        
         let task = Process()
-        task.launchPath = BasePath + type.rawValue
-        task.launch()
-        task.waitUntilExit()
+//        task.launchPath = type.rawValue
+//        task.launch()
+//        task.waitUntilExit()
         return Int(task.terminationStatus)
     }
 }
