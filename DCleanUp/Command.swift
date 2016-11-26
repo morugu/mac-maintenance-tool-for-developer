9bc37600c4ac5a07ce95469473c8e4c2020af6e0
shoya <12c1055@gmail.com>
2016-11-26 23:50:18 +0900

    Add command define

--- DCleanUp/Command.swift
+++ DCleanUp/Command.swift
@@ -11,7 +11,10 @@ import Cocoa
 enum CommandType: String {
     case
     Env = "/usr/bin/env",
-    Purge = "/usr/sbin/purge"
+    Purge = "/usr/sbin/purge",
+    DeleteDerivedData = " rm -rf ~/Library/Developer/Xcode/DerivedData",
+    DeleteArchives = "rm -rf ~/Library/Developer/Xcode/Archives",
+    DeleteCaches = "rm -rf ~/Library/Caches"
 }
 
 class Command {
@@ -22,13 +25,14 @@ class Command {
         print(type.rawValue)
         
         // TODO: http://stackoverflow.com/questions/26707557/how-to-execute-a-shell-command-with-root-permission-from-swift
-        NSAppleScript(source: "do shell script \"sudo \(type.rawValue)\" with administrator " +
+        NSAppleScript(source: "do shell script \"sudo env\" with administrator " +
             "privileges")!.executeAndReturnError(nil)
         
-        let task = Process()
+//        let task = Process()
 //        task.launchPath = type.rawValue
 //        task.launch()
 //        task.waitUntilExit()
-        return Int(task.terminationStatus)
+//        return Int(task.terminationStatus)
+        return 0
     }
 }
