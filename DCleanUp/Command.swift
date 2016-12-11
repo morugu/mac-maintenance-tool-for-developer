3b633eb37197e34189f86ea489cb17142a5dc4e6
shoya <12c1055@gmail.com>
2016-12-11 16:36:43 +0900

    Fix finish event handling

--- DCleanUp/Command.swift
+++ DCleanUp/Command.swift
@@ -57,29 +57,16 @@ enum CommandType: CommandExtention  {
 }
 
 class Command {
-    
-    static let BasePath = "/usr/bin/"
-    
+
     static func execute(type: CommandType) -> Int {
-//        print(type.rawValue)
-        
-//         TODO: http://stackoverflow.com/questions/26707557/how-to-execute-a-shell-command-with-root-permission-from-swift
-//        let script = "do shell script \"\(type.rawValue)\" with administrator privileges";
-//        NSAppleScript(source: script)!.executeAndReturnError(nil)
-//        print(script)
         if let path = Bundle.main.path(forResource: type.scriptFileName, ofType:"scpt") {
             let task = Process()
             task.launchPath = "/usr/bin/osascript"
             task.arguments = [path]
             task.launch()
+            task.waitUntilExit()
+            return Int(task.terminationStatus)
         }
-        
-//        let task = Process()
-//        task.launchPath = type.rawValue
-//        task.currentDirectoryPath = "~/Desktop/SpotifyTimer 2016-11-13 16-38-52"
-//        task.launch()
-//        task.waitUntilExit()
-//        return Int(task.terminationStatus)
-        return 0
+        return 1
     }
 }
